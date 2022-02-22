4d5ac52b526e56c5811ec7953c3c621ec467e827
Alex13090 <kohninnaung@gmail.com>
2022-02-22 16:02:46 +0100

    on:click button view

--- index.js
+++ index.js
@@ -2,15 +2,38 @@
 const monApp = Vue.createApp({
     data() {
         return {
-            inputValue: ''
+            inputValue1: '',
+            inputValue2: '',
+            view: true
         };
     },
 
+    computed: {
+        allClass(){
+            if(this.inputValue1 === 'hello'){
+                return 'hello';
+            }
+
+            if(this.inputValue1 === 'world'){
+                return 'world';
+            }
+
+            if(this.view == true){
+                return 'display';
+            }
+
+            if(this.view == false){
+                return 'hidden';
+            }
+        }
+    },
+
     methods: {
-        getInputValue(event){
-            this.inputValue = event.target.value;
+        show(){
+            this.view = !this.view;
         }
     }
+
 });
 
 monApp.mount('#app');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
